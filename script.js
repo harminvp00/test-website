c86cbb629372a3b06611a0169b9cdffe70301350
Harmin Vekariya <132663894+harminvp00@users.noreply.github.com>
2025-07-10 19:07:39 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById('sideMenu');
-    menu.classList.toggle('active');
-}
-
-function sendToWhatsApp() {
-    window.open(`https://wa.me/918200571458?text=${"Hello! I'm "}`, '_blank');
-}
